59127e6e5edb08ebcac24a97e997a476243af5d2
George Samuel Igoe <george.igoe@gmail.com>
2018-06-13 19:07:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,13 +9,10 @@ function takeANumber(katzDeliLine, name) {
 function nowServing(deliLine) {
   if(deliLine.length > 0) {
     return (`Currently serving ${deliLine.shift()}.`)
-    
   }   else {return `There is nobody waiting to be served!`;
-    
       }
-  
   
 }
 
-  
+function currentLine
 
